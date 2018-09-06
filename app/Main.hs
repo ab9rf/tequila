6103d4fe21693b408ea2f5e045eee6bc4f832ed7
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2018-09-06 00:33:01 -0500

    maintain explict renderer state

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,27 @@ import SDL
 import Data.Word
 import System.Environment (getArgs)
 import Control.Monad (unless)
+import Control.Monad.State
+import Control.Monad.Zip
+
+data RendererState = RendererState { 
+      rsWindow :: Window
+    , rsRenderer :: Renderer
+    , rsTileSet :: Texture
+    , rsTileSetInfo :: TextureInfo
+    , rsTileSize :: V2 Int
+    } 
+  deriving Eq
+  
+type RSIO a = StateT RendererState IO a  
+
+findTileSize :: TextureInfo -> V2 Int
+findTileSize tilesetInfo = 
+    let tilesPerRow = 16
+        tilesPerColumn = 256 `div` tilesPerRow
+        tileWidth = (fromIntegral (textureWidth tilesetInfo)) `div` tilesPerRow
+        tileHeight = (fromIntegral (textureHeight tilesetInfo)) `div` tilesPerColumn
+    in V2 tileWidth tileHeight
 
 main :: IO ()
 main = do
@@ -14,59 +35,68 @@ main = do
     initializeAll
     window <- createWindow "SDL Test" defaultWindow { windowVisible = False }
     renderer <- createRenderer window (-1) defaultRenderer
-    startTick <- ticks
     tileset <- loadImg renderer (head args)
     tilesetInfo <- queryTexture tileset
+    let tileSize = findTileSize tilesetInfo 
+        winSize = fmap fromIntegral (tileSize * (V2 80 25))
+        renderState = RendererState window renderer tileset tilesetInfo tileSize
+      in runStateT (do 
+          windowSize window $= winSize
+          rendererLogicalSize renderer $= Just winSize
+          showWindow window
+          mainLoop
+        ) renderState
+    return ()
+
+mainLoop :: RSIO ()
+mainLoop =        
     let 
-      tilesPerRow = 16
-      tilesPerColumn = 256 `div` tilesPerRow
-      tileWidth = (fromIntegral (textureWidth tilesetInfo)) `div` tilesPerRow
-      tileHeight = (fromIntegral (textureHeight tilesetInfo)) `div` tilesPerColumn
-      screenWidth = 80
-      screenHeight = 25
-      windowWidth = screenWidth * tileWidth
-      windowHeight = screenHeight * tileHeight
       eventIsQPress event = 
                 case eventPayload event of
                   KeyboardEvent ke -> 
                       keyboardEventKeyMotion ke == Pressed &&
                       keysymKeycode (keyboardEventKeysym ke) == KeycodeQ
                   _ -> False
-      drawScreen :: Renderer -> Texture -> Screen -> IO ()    
-      drawScreen r tileset screen = 
-        let
-          coords = [ V2 x y | x <- [0..screenWidth], y <- [0..screenHeight] ]
-          drawOne coord@(V2 x y) = 
-            let 
-              (Cell ch fg bg) = screen (fmap fromIntegral coord)
-              tileCoord = fmap fromIntegral $ V2 
-                ((fromIntegral ch) `mod` tilesPerRow) 
-                ((fromIntegral ch) `div` tilesPerRow) 
-              tileSz = V2 tileWidth tileHeight
-              srcRect = Rectangle (P (tileCoord * tileSz)) tileSz
-              dstRect = Rectangle (P (coord * tileSz)) tileSz
-            in do
-              textureColorMod tileset $= noAlpha fg
-              rendererDrawColor r $= bg
-              fillRect r (Just dstRect)
-              rendererDrawBlendMode r $= BlendAlphaBlend
-              copy r tileset (Just srcRect) (Just dstRect) 
-        in
-          mapM_ drawOne coords
       mainLoop = 
         do
+          renderer <- rsRenderer <$> Control.Monad.State.get 
           events <- pollEvents
           clear renderer
-          drawScreen renderer tileset debugScreen
+          drawScreen debugScreen
           present renderer
           unless (any eventIsQPress events) mainLoop
       in do 
-        windowSize window $= (V2 windowWidth windowHeight)
-        rendererLogicalSize renderer $= Just (V2 windowWidth windowHeight)
-        showWindow window
         mainLoop
-    
-loadImg :: Renderer -> String -> IO Texture
+        
+drawScreen :: Screen -> RSIO ()        
+drawScreen screen = do
+  rs <- Control.Monad.State.get
+  winSize <- SDL.get (windowSize (rsWindow rs))
+  let 
+    r = rsRenderer rs
+    tileset = rsTileSet rs
+    tileSz = fmap fromIntegral (rsTileSize rs)
+    V2 screenWidth screenHeight = mzipWith div winSize tileSz
+    coords = [ V2 x y | x <- [0..screenWidth], y <- [0..screenHeight] ]
+    drawOne coord@(V2 x y) = 
+      let 
+        tilesPerRow = 16
+        (Cell ch fg bg) = screen (fmap fromIntegral coord)
+        tileCoord = fmap fromIntegral $ V2 
+          ((fromIntegral ch) `mod` tilesPerRow) 
+          ((fromIntegral ch) `div` tilesPerRow) 
+        srcRect = Rectangle (P (tileCoord * tileSz)) tileSz
+        dstRect = Rectangle (P (coord * tileSz)) tileSz
+      in do
+        textureColorMod tileset $= noAlpha fg
+        rendererDrawColor r $= bg
+        fillRect r (Just dstRect)
+        rendererDrawBlendMode r $= BlendAlphaBlend
+        copy r tileset (Just srcRect) (Just dstRect) 
+    in
+      mapM_ drawOne coords
+        
+loadImg :: (MonadIO m) => Renderer -> String -> m Texture
 loadImg r file = do
     surf <- loadBMP file
     surfaceColorKey surf $= Just magenta
